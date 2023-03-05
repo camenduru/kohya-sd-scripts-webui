f314d50cd8993318f75b0616962cb392d7eab9ac
camenduru <54370274+camenduru@users.noreply.github.com>
2023-03-05 13:38:56 +0300

    kohya-ss v0.4.4

--- install.py
+++ install.py
@@ -75,7 +75,7 @@ def prepare_environment():
         launch.run(f"cd \"{repo_dir}\" && {launch.git} reset --hard origin/main")
     else:
         sd_scripts_repo_commit_hash = os.environ.get('SD_SCRIPTS_COMMIT_HASH', "f0ae7eea950b93b1550dc34a782da3bafda2a8c0")
-        sd_scripts_repo = os.environ.get('SD_SCRIPTS_REPO', "https://github.com/camenduru/sd-scripts")
+        sd_scripts_repo = os.environ.get('SD_SCRIPTS_REPO', "https://github.com/kohya-ss/sd-scripts")
         launch.git_clone(sd_scripts_repo, os.path.join(scripts.basedir(), "extensions", "kohya-sd-scripts-webui", "kohya_ss"), "SD Scripts", sd_scripts_repo_commit_hash)
 
     if not launch.is_installed("gradio"):
